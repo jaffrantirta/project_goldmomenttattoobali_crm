65338a46ed829d94fcfc7c8357417fafe4d9406c
jaffrantirta <franartika@gmail.com>
2026-03-10 00:31:04 +0800

    analityc

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,24 @@
-import type { Metadata } from 'next'
-import { Geist } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Geist } from "next/font/google";
+import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
-const geist = Geist({ subsets: ['latin'] })
+const geist = Geist({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Gold Moment Tattoo Bali',
-  description: 'Book your tattoo with Gold Moment Tattoo Bali',
-}
+  title: "Gold Moment Tattoo Bali",
+  description: "Book your tattoo with Gold Moment Tattoo Bali",
+};
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
+      <Analytics />
       <body className={`${geist.className} antialiased`}>{children}</body>
     </html>
-  )
+  );
 }
